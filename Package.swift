658d0abb1910d65959907ea611d4d14fa459269b
martinlasek <martinlasek@hotmail.de>
2017-12-28 23:50:56 +0100

    updated package syntax to swift 4

--- Package.swift
+++ Package.swift
@@ -1,24 +1,30 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "vaporberlinBE",
-    targets: [
-      Target(name: "App"),
-      Target(name: "Run", dependencies: ["App"]),
+    products: [
+      .library(name: "App", targets: ["App"]),
+      .executable(name: "Run", targets: ["Run"])
     ],
     dependencies: [
-      .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-      .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
-      .Package(url: "https://github.com/vapor/auth-provider.git", majorVersion: 1),
-      .Package(url: "https://github.com/vapor/postgresql-provider.git", majorVersion: 2),
-      .Package(url: "https://github.com/vapor/leaf-provider.git", majorVersion: 1)
+      .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
+      .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.3.0")),
+      .package(url: "https://github.com/vapor/auth-provider.git", .upToNextMajor(from: "1.2.0")),
+      .package(url: "https://github.com/vapor/postgresql-provider.git", .upToNextMajor(from: "2.1.0")),
+      .package(url: "https://github.com/vapor/leaf-provider.git", .upToNextMajor(from: "1.1.0"))
     ],
-    exclude: [
-      "Config",
-      "Database",
-      "Localization",
-      "Public",
-      "Resources",
+    targets: [
+      .target(name: "App", dependencies: ["Vapor", "FluentProvider", "AuthProvider", "PostgreSQLProvider", "LeafProvider"],
+        exclude: [
+          "Config",
+          "Public",
+          "Resources",
+        ]
+      ),
+      .target(name: "Run", dependencies: ["App"]),
+      .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
     ]
 )
 
